8726b26633b613e6a5021f98fe8776b374384b03
ninazu <ninazu@ukr.net>
2017-10-06 15:47:29 +0300

    #start

--- examples/reCaptcha.go
+++ examples/reCaptcha.go
@@ -8,12 +8,12 @@ import (
 func main() {
 	//TODO REMOVE KEY
 
-	id, err := ninazu.ReCaptcha("123").
+	token, err := ninazu.ReCaptcha("123").
 		GetCaptchaSolution("6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-", "https://www.google.com/recaptcha/api2/demo")
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(id)
+	fmt.Println(token)
 }
