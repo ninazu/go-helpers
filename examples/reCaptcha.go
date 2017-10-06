50c8ddf9c27a394ffabfc539c5aebe2dcf5fb4f6
ninazu <ninazu@ukr.net>
2017-10-06 15:45:38 +0300

    #start

--- examples/reCaptcha.go
+++ examples/reCaptcha.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"github.com/ninazu/go-helpers"
+	"../../go-helpers"
 	"fmt"
 )
 
 func main() {
-	r := ninazu.ReCaptcha("1abc234de56fab7c89012d34e56fa7b8").
-		GetCaptcha("6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-", "https://www.google.com/recaptcha/api2/demo")
+	//TODO REMOVE KEY
 
-	fmt.Println(r)
+	id, err := ninazu.ReCaptcha("123").
+		GetCaptchaSolution("6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-", "https://www.google.com/recaptcha/api2/demo")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(id)
 }
--- reCaptcha.go
+++ reCaptcha.go
@@ -3,31 +3,123 @@ package ninazu
 import (
 	"net/url"
 	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"time"
+	"strconv"
+	"errors"
 )
 
 type reCaptcha struct {
-	key string
+	key      string
+	turnSend int
+	turnWait int
+	turnGet  int
 }
 
-type response struct {
-	Id    string
-	Token string
+type ruCaptchaResponse struct {
+	Status  int
+	Request string
 }
 
 func ReCaptcha(key string) (*reCaptcha) {
 	return &reCaptcha{
-		key: key,
+		key:      key,
+		turnSend: 0,
+		turnWait: 0,
+		turnGet:  0,
 	}
 }
 
-func sendRequest(url string) (*http.Response) {
-	resp, err := http.Get(url)
+func (r *reCaptcha) GetCaptchaSolution(captchaKey, pageUrl string) (string, error) {
+	r.turnGet++
+
+	id, err := r.sendJob(captchaKey, pageUrl)
 
 	if err != nil {
-		return nil
+		return r.returnTurnGet("", err)
 	}
 
-	return resp
+	//Wait first solution
+	time.Sleep(20 * time.Second)
+	sol, err := r.waitSolution(id)
+
+	if err != nil {
+
+		return r.returnTurnGet("", err)
+	} else if len(sol) == 0 {
+		return r.GetCaptchaSolution(captchaKey, pageUrl)
+	}
+
+	return sol, nil
+}
+
+func (r *reCaptcha) trySendJob(url string) (int, error) {
+	if r.turnSend > 10 {
+		return r.returnTurnSend(0, errors.New("MAX_TURN"))
+	}
+
+	r.turnSend++
+
+	d := sendRequest(url)
+
+	if d.Status == 0 {
+		switch d.Request {
+		case "MAX_USER_TURN":
+			time.Sleep(1 * time.Second)
+
+			return r.trySendJob(url)
+
+		case "ERROR_NO_SLOT_AVAILABLE":
+			time.Sleep(5 * time.Second)
+
+			return r.trySendJob(url)
+
+		case "ERROR_ZERO_BALANCE":
+			time.Sleep(60 * time.Second)
+
+			return r.trySendJob(url)
+
+		default:
+			return r.returnTurnSend(0, errors.New(d.Request))
+		}
+	}
+
+	i, err := strconv.Atoi(d.Request)
+
+	if err != nil {
+		r.returnTurnSend(0, err)
+	}
+
+	return r.returnTurnSend(i, nil)
+}
+
+func (r *reCaptcha) tryWaitSolution(url string) (string, error) {
+	if r.turnWait > 20 {
+		return r.returnTurnWait("", errors.New("MAX_TURN"))
+	}
+
+	r.turnWait++
+
+	d := sendRequest(url)
+
+	if d.Status == 0 {
+		switch d.Request {
+		case "CAPCHA_NOT_READY":
+			time.Sleep(5 * time.Second)
+
+			return r.tryWaitSolution(url)
+
+		case "ERROR_CAPTCHA_UNSOLVABLE":
+		case "ERROR_BAD_DUPLICATES":
+			return r.returnTurnWait("", nil)
+
+		default:
+			return r.returnTurnWait("", errors.New(d.Request))
+		}
+	}
+
+	return r.returnTurnWait(d.Request, nil)
 }
 
 func (r *reCaptcha) buildUrl(script string, v *url.Values) string {
@@ -37,25 +129,57 @@ func (r *reCaptcha) buildUrl(script string, v *url.Values) string {
 	return "http://rucaptcha.com/" + script + "?" + v.Encode()
 }
 
-func (r *reCaptcha) ReportBadCaptcha(id string) {
-	v := url.Values{}
-	v.Set("method", "userrecaptcha")
-	v.Set("action", "get")
-	v.Set("id", id)
-
-	sendRequest(r.buildUrl("res.php", &v))
-}
-
-func (r *reCaptcha) GetCaptcha(captchaKey, pageUrl string) *response {
+func (r *reCaptcha) sendJob(captchaKey, pageUrl string) (int, error) {
 	v := url.Values{}
 	v.Set("method", "userrecaptcha")
 	v.Set("googlekey", captchaKey)
 	v.Set("pageurl", pageUrl)
 
-	sendRequest(r.buildUrl("in.php", &v))
+	return r.trySendJob(r.buildUrl("in.php", &v))
+}
 
-	return &response{
-		Id:    "1",
-		Token: "Hello",
+func (r *reCaptcha) waitSolution(id int) (string, error) {
+	v := url.Values{}
+	v.Set("method", "userrecaptcha")
+	v.Set("action", "get")
+	v.Set("id", strconv.Itoa(id))
+
+	return r.tryWaitSolution(r.buildUrl("res.php", &v))
+}
+
+func (r *reCaptcha) returnTurnGet(s string, e error) (string, error) {
+	r.turnGet = 0
+
+	return s, e
+}
+
+func (r *reCaptcha) returnTurnWait(s string, e error) (string, error) {
+	r.turnWait = 0
+
+	return s, e
+}
+
+func (r *reCaptcha) returnTurnSend(s int, e error) (int, error) {
+	r.turnSend = 0
+
+	return s, e
+}
+
+func sendRequest(url string) (*ruCaptchaResponse) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return nil
 	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil
+	}
+
+	var data ruCaptchaResponse
+	json.Unmarshal(body, &data)
+
+	return &data
 }
